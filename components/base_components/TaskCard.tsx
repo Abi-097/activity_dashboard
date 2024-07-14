22982c308c0d7b6203c0886a88819a8c2c56bf8b
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-07-14 11:51:13 +0530

    taskcard created and mapped

--- components/base_components/TaskCard.tsx
+++ components/base_components/TaskCard.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import {
   Select,
@@ -16,7 +9,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-const TaskCard: React.FC<{ data: string }> = ({ data }) => {
+
+interface TaskCardProps {
+  data: {
+    created: string;
+    time: string;
+    preparedFor: string;
+    description: string;
+    reminder: string;
+    task: string;
+    user: string;
+  };
+}
+const TaskCard: React.FC<TaskCardProps> = ({ data }) => {
   return (
     <div>
       <div>
@@ -31,66 +36,72 @@ const TaskCard: React.FC<{ data: string }> = ({ data }) => {
           <p className="ml-12">{data.description}</p>{" "}
           <div>
             <div className="flex flex-wrap mt-4">
-              <div className="w-full md:w-1/2 lg:w-1/3 border border-gray-300 p-4">
-                <p className="ml-3">Reminder</p>
-                <span>
-                  <Select defaultValue={data.reminder}>
+              {data.reminder && (
+                <div className="w-full md:w-1/2 lg:w-1/3 border border-gray-300 p-4">
+                  <p className="ml-3">Reminder</p>
+                  <span>
+                    <Select value={data.reminder}>
+                      <SelectTrigger className="w-[180px] border-none">
+                        <SelectValue placeholder="Select a reminder" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="noneReminder">
+                            No Reminder
+                          </SelectItem>
+                          <SelectItem value="oneHReminder">
+                            One Hour reminder
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </span>
+                </div>
+              )}
+              {data.task && (
+                <div className="w-full md:w-1/2 lg:w-1/3 border border-gray-300 p-4">
+                  <p className="ml-3">Task Priority</p>
+                  <span>
+                    <Select value={data.task}>
+                      <SelectTrigger className="w-[180px] border-none">
+                        <SelectValue placeholder="Select a Task Priority" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="high">
+                            <span className=" flex items-center">
+                              <span className=" left-0 inline-flex h-5 w-5 rounded-full bg-red-400 mr-2" />
+                              <p className="text-slate-400">High</p>
+                            </span>
+                          </SelectItem>
+                          <SelectItem value="low">
+                            <span className=" flex items-center">
+                              <span className=" left-0 inline-flex h-5 w-5 rounded-full bg-green-400 mr-2" />
+                              <p className="text-slate-400">Low</p>
+                            </span>
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </span>
+                </div>
+              )}
+              {data.user && (
+                <div className="w-full md:w-full lg:w-1/3 border border-gray-300 p-4">
+                  <p className="ml-3">Assign To</p>
+                  <Select value={data.user}>
                     <SelectTrigger className="w-[180px] border-none">
-                      <SelectValue placeholder="Select a reminder" />
+                      <SelectValue placeholder="Select a User" />
                     </SelectTrigger>
                     <SelectContent>
                       <SelectGroup>
-                        <SelectItem value="noneReminder">
-                          No Reminder
-                        </SelectItem>
-                        <SelectItem value="oneHReminder">
-                          One Hour reminder
-                        </SelectItem>
+                        <SelectItem value="lucy">Lucy Lockwood</SelectItem>
+                        <SelectItem value="im">Iron Man</SelectItem>
                       </SelectGroup>
                     </SelectContent>
                   </Select>
-                </span>
-              </div>
-              <div className="w-full md:w-1/2 lg:w-1/3 border border-gray-300 p-4">
-                <p className="ml-3">Task Priority</p>
-                <span>
-                  <Select defaultValue={data.task}>
-                    <SelectTrigger className="w-[180px] border-none">
-                      <SelectValue placeholder="Select a Task Priority" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem value="high">
-                          <span className=" flex items-center">
-                            <span className=" left-0 inline-flex h-5 w-5 rounded-full bg-red-400 mr-2" />
-                            <p className="text-slate-400">High</p>
-                          </span>
-                        </SelectItem>
-                        <SelectItem value="low">
-                          <span className=" flex items-center">
-                            <span className=" left-0 inline-flex h-5 w-5 rounded-full bg-green-400 mr-2" />
-                            <p className="text-slate-400">Low</p>
-                          </span>
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </span>
-              </div>
-              <div className="w-full md:w-full lg:w-1/3 border border-gray-300 p-4">
-                <p className="ml-3">Assign To</p>
-                <Select defaultValue={data.user}>
-                  <SelectTrigger className="w-[180px] border-none">
-                    <SelectValue placeholder="Select a User" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectItem value="lucy">Lucy Lockwood</SelectItem>
-                      <SelectItem value="im">Iron Man</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </div>
+                </div>
+              )}
             </div>
           </div>
         </Card>
